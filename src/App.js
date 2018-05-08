b4b6cf1c1680880b88fbc040ef336c0109c169ae
Laís Lima <laislima98@hotmail.com>
2018-05-07 21:55:47 -0300

    Implementando o router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
-import Perfil from './Components/Perfil.js';
-// import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Login from './Components/Login';
+import Perfil from './Components/Perfil';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Perfil/>
-      </div>
+      <Router>
+        <div>
+          <Route exac path="/Login" component={Login} />
+          <Route  exac path="/" component={Perfil} />
+        </div>
+      </Router>
     );
   }
 }
